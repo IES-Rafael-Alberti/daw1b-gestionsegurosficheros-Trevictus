1213c5cae7d916e19152f034cd64bb8ed21da460
vgomtej353 <gtvictor@iesrafaelalberti.es>
2025-04-01 13:31:51 +0200

    TODO: capa ui pocos avances

--- src/main/kotlin/ui/Consola.kt
+++ src/main/kotlin/ui/Consola.kt
@@ -3,19 +3,52 @@ package ui
 class Consola: IEntradaSalida {
 
     override fun mostrar(msj: String, salto: Boolean, pausa: Boolean) {
-        TODO("Not yet implemented")
+
+        if(salto){
+            println(msj)
+        }else{
+            print(msj)
+        }
+        if(pausa){
+            println("Pulsa Enter para continuar...")
+            readln()
+        }
     }
 
     override fun mostrarError(msj: String, pausa: Boolean) {
-        TODO("Not yet implemented")
+        val error = "ERROR - "
+        if(msj.startsWith(error)){
+            println(msj)
+        }else{
+            println(error + msj)
+        }
+        if(pausa){
+            println("Pulsa Enter para continuar...")
+            readln()
+        }
     }
 
     override fun pedirInfo(msj: String): String {
-        TODO("Not yet implemented")
+        if(msj.isNotEmpty()){
+            println(msj)
+        }
+        val entrada = readln()
+        return entrada.trim()
     }
 
     override fun pedirInfo(msj: String, error: String, debeCumplir: (String) -> Boolean): String {
-        TODO("Not yet implemented")
+        if(msj.isNotEmpty()){
+            println(msj)
+        }
+        var entrada = readlnOrNull()?.trim() ?: ""
+        while (!debeCumplir(entrada)) {
+            println(error)
+            if (msj.isNotEmpty()) {
+                println(msj)
+            }
+            entrada = readlnOrNull()?.trim() ?: ""
+        }
+        return entrada
     }
 
     override fun pedirDouble(
@@ -24,7 +57,7 @@ class Consola: IEntradaSalida {
         errorConv: String,
         debeCumplir: (Double) -> Boolean
     ): Double {
-        TODO("Not yet implemented")
+        val numDecimal
     }
 
     override fun pedirEntero(prompt: String, error: String, errorConv: String, debeCumplir: (Int) -> Boolean): Int {
