fb8c95f4767a96240782b0a803a681523e540c50
vgomtej353 <gtvictor@iesrafaelalberti.es>
2025-04-02 13:00:01 +0200

    commit ficheros finalizaado TODO: capa app

--- src/main/kotlin/utils/Ficheros.kt
+++ src/main/kotlin/utils/Ficheros.kt
@@ -3,25 +3,43 @@ package utils
 import model.IExportable
 import model.Seguro
 import ui.IEntradaSalida
+import java.io.File
 
-class Ficheros(val entradaSalida: IEntradaSalida):IUtilFicheros {
+class Ficheros(val entradaSalida: IEntradaSalida) : IUtilFicheros {
     override fun leerArchivo(ruta: String): List<String> {
-        TODO("Not yet implemented")
+        val cadenaArchivo = File(ruta).readLines()
+        if (!existeFichero(ruta)) {
+            entradaSalida.mostrarError("ERROR. No se pudo leer el archivo.")
+            return emptyList()
+        }
+        return cadenaArchivo
     }
 
     override fun agregarLinea(ruta: String, linea: String): Boolean {
-        TODO("Not yet implemented")
+        try {
+            val archivo = File(ruta)
+            if (!existeFichero(ruta)) {
+                archivo.writeText(linea + "\n")
+            } else {
+                archivo.appendText(linea + "\n")
+            }
+            return true
+        } catch (e: Exception) {
+            println("ERROR. Problemas al escribir en/el archivo. ${e.message}")
+            return false
+        }
     }
 
     override fun <T : IExportable> escribirArchivo(ruta: String, elementos: List<T>): Boolean {
-        TODO("Not yet implemented")
+        File(ruta).writeText(elementos.joinToString("\n") { it.serializar() })
+        return true
     }
 
     override fun existeFichero(ruta: String): Boolean {
-        TODO("Not yet implemented")
+        return File(ruta).exists()
     }
 
     override fun existeDirectorio(ruta: String): Boolean {
-        TODO("Not yet implemented")
+        return File(ruta).isDirectory
     }
 }
